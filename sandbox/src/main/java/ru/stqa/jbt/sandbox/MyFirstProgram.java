651437f87a3573dfd018bb5585f23cc117bfe4dd
Zinnurka <32505382+Zinnurka@users.noreply.github.com>
2020-04-09 21:49:31 +0500

    Изучил суммы

--- sandbox/src/main/java/ru/stqa/jbt/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/jbt/sandbox/MyFirstProgram.java
@@ -1,7 +1,15 @@
 package ru.stqa.jbt.sandbox;
 
 public class MyFirstProgram {
-    public static void main (String[] args ){
+    public static void main(String[] args) {
         System.out.println("Привет, друг!");
+
+        System.out.println(2+2);
+        System.out.println(2*2);
+        System.out.println(2-2);
+        System.out.println(2/2);
+        System.out.println(1/2);
+        System.out.println("2"+"2");
+        
     }
 }
