37c88f0c01fa69fa800af7c77a4305b61b328d98
Zinnurka <32505382+Zinnurka@users.noreply.github.com>
2020-04-09 22:19:24 +0500

    Восстановил, фух

--- sandbox/src/main/java/ru/stqa/jbt/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/jbt/sandbox/MyFirstProgram.java
@@ -1,15 +1,20 @@
 package ru.stqa.jbt.sandbox;
 
 public class MyFirstProgram {
-    public static void main(String[] args) {
-        System.out.println("Привет, друг!");
 
-        System.out.println(2+2);
-        System.out.println(2*2);
-        System.out.println(2-2);
-        System.out.println(2/2);
-        System.out.println(1/2);
-        System.out.println("2"+"2");
 
-    }
+        public static void main (String[] args ){
+            System.out.println("Привет, друг!");
+
+
+            System.out.println(2+2);
+            System.out.println(2*2);
+            System.out.println(2-2);
+            System.out.println(2/2);
+            System.out.println(1/2);
+            System.out.println("2"+"2");
+
+ }
 }
+
+
