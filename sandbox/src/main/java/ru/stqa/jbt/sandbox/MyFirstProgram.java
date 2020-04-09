f60b04b7485b3f4ab25e6e283ace6b519792f7fa
Zinnurka <32505382+Zinnurka@users.noreply.github.com>
2020-04-09 22:55:04 +0500

    Функции

--- sandbox/src/main/java/ru/stqa/jbt/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/jbt/sandbox/MyFirstProgram.java
@@ -3,18 +3,28 @@ package ru.stqa.jbt.sandbox;
 public class MyFirstProgram {
 
 
-        public static void main (String[] args ){
-            System.out.println("Привет, друг!");
+        public static void main (String[] args ) {
+            hello("Рамиль");
+            double len = 5;
+            double a = 4;
+            double b = 3;
+            System.out.println(area(len));
+            System.out.println(area(a,b));
+        }
 
+        public static void hello(String name) {
 
-            System.out.println(2+2);
-            System.out.println(2*2);
-            System.out.println(2-2);
-            System.out.println(2/2);
-            System.out.println(1/2);
-            System.out.println("2"+"2");
+            System.out.println("Привет," + name);
+        }
+
+        public static double area (double l){
+            return l*l;
+        }
+        public static double area (double a, double b){
+            return a*b;
+        }
 
  }
-}
+
 
 
