6c454e8a93469e2a22641d88542fe3aa5e36d7f2
Zinnurka <32505382+Zinnurka@users.noreply.github.com>
2020-04-10 11:04:37 +0500

    TestNG

--- sandbox/src/main/java/ru/stqa/jbt/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/jbt/sandbox/MyFirstProgram.java
@@ -3,28 +3,21 @@ package ru.stqa.jbt.sandbox;
 public class MyFirstProgram {
 
 
-        public static void main (String[] args ) {
-            hello("Рамиль");
-            double len = 5;
-            double a = 4;
-            double b = 3;
-            System.out.println(area(len));
-            System.out.println(area(a,b));
-        }
+    public static void main(String[] args) {
+        hello("Рамиль");
+        Square s = new Square(5);
+        Rectandle r = new Rectandle(3, 4);
+        System.out.println(s.area());
+        System.out.println(r.area());
+    }
 
-        public static void hello(String name) {
+    public static void hello(String name) {
 
-            System.out.println("Привет," + name);
-        }
+        System.out.println("Привет," + name);
+    }
 
-        public static double area (double l){
-            return l*l;
-        }
-        public static double area (double a, double b){
-            return a*b;
-        }
+}
 
- }
 
 
 
---
+++ sandbox/src/main/java/ru/stqa/jbt/sandbox/Rectandle.java
@@ -0,0 +1,16 @@
+package ru.stqa.jbt.sandbox;
+
+public class Rectandle {
+    public double a;
+    public double b;
+    public Rectandle(double a, double b){
+        this.a=a;
+        this.b=b;
+    }
+    public double area() {
+        return this.a * this.b;
+    }
+
+}
+
+
